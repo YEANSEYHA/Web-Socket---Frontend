8ec25f3f3b7d3a5b5ba4734cac2fe63a8adbf32b
Yean Seyha <yeanseyha7@gmail.com>
2023-12-16 22:38:34 +0700

    Init Web Socket in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+
+    newSocket.on("message", (message) => {
+      if (Array.isArray(message)) {
+        setMessages(message);
+      } else {
+        setMessages((prevMessages) => [...prevMessages, message]);
+      }
+    });
+
+    newSocket.on("users", (userList) => {
+      setUsers(userList);
+    });
+
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = (message) => {
+    if (socket) {
+      socket.emit("chatMessage", message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Hello</h1>
+        <div>
+          <h2>Chat Room</h2>
+          <div>
+            <h3>Users:</h3>
+            <ul>
+              {users.map((user) => (
+                <li key={user.id}>{user.id}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h3>Messages:</h3>
+            <ul>
+              {messages.map((message, index) => (
+                <li key={index}>
+                  {message.user ? `${message.user}: ${message.text}` : message}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <input
+            type="text"
+            placeholder="Type a message..."
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                sendMessage(e.target.value);
+                e.target.value = "";
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
